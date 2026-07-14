fef6df5d2e2b15a08ab0db077a56860f56003bc8
Durga Prasad Patra <144362104+DurgaPrasadPatra@users.noreply.github.com>
2026-07-14 15:25:49 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
 
    backend "s3" {
 
-    bucket         = "durgadevops-terraform-state"
+    bucket         = "durgadevops-terraform-state-bk"
     key            = "dev/project/terraform.tfstate"
     region         = "ap-south-1"
 
