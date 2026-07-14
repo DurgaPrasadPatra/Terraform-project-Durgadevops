9d2f5408fc4be04dbcdaa295ea82bc8cb3c9da79
Durga Prasad Patra <144362104+DurgaPrasadPatra@users.noreply.github.com>
2026-07-14 15:06:52 +0530

    Initialize AWS provider in provider.tf
    Purpose
    
    Defines the cloud provider used by Terraform and specifies the provider version and AWS region.
    
    Why it is needed
    Connects Terraform to AWS.
    Downloads the AWS provider plugin.
    Ensures version consistency across environments.
    Applies default tags to all supported resources.
    Contains
    required_providers
    AWS Provider
    Default Tags

---
+++ provider.tf
@@ -0,0 +1,26 @@
+terraform {
+
+  required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+
+    }
+
+  }
+
+}
+
+provider "aws" {
+
+  region = var.aws_region
+
+  default_tags {
+
+    tags = local.common_tags
+
+  }
+
+}
