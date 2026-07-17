f31e09f44bff53ceb502a2bb8e00e042768a624e
Durga Prasad Patra <144362104+DurgaPrasadPatra@users.noreply.github.com>
2026-07-17 20:56:13 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,42 @@
+resource "aws_instance" "web-server-1" {
+
+  ami = var.ami_id
+
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.public.id
+
+  vpc_security_group_ids = [
+
+    aws_security_group.web_sg.id
+
+  ]
+
+  key_name = var.key_name
+
+  root_block_device {
+
+    volume_size = 20
+
+    volume_type = "gp3"
+
+    encrypted = true
+
+  }
+
+  tags = {
+
+    Name = "${var.project_name}-server"
+
+  }
+
+}
+
+
+
+
+
+
+
+
+#author- Durga Prasad Patra
