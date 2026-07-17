0413bb13fd6bb4517e1191036798cf40bc6e0157
Durga Prasad Patra <144362104+DurgaPrasadPatra@users.noreply.github.com>
2026-07-17 21:41:30 +0530

    Update RDS username and password to use local secrets that decode AWS secret manager

--- rds.tf
+++ rds.tf
@@ -22,9 +22,8 @@ resource "aws_db_instance" "mysql" {
 
   db_name = var.db_name
 
-  username = var.db_username
-
-  password = var.db_password
+  username = local.db_secret.username
+  password = local.db_secret.password
 
   skip_final_snapshot = true
 
