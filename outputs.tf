06762761c9e190b9cd0f2172b74448eb2160edbc
Durga Prasad Patra <144362104+DurgaPrasadPatra@users.noreply.github.com>
2026-07-17 20:58:30 +0530

    Fix formatting in outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,33 @@
+output "vpc_id" {
+
+  value = aws_vpc.main.id
+
+}
+
+output "public_subnet" {
+
+  value = aws_subnet.public.id
+
+}
+
+output "private_subnet" {
+
+  value = aws_subnet.private.id
+
+}
+
+output "ec2_public_ip" {
+
+  value = aws_instance.web.public_ip
+
+}
+
+output "rds_endpoint" {
+
+  value = aws_db_instance.mysql.endpoint
+
+}
+
+
+
+#Author-DurgaPrasadPatra
